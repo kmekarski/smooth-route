1ae0d70b22ad021c52cef0701eefae3690c2115a
kmekarski <72306134+kmekarski@users.noreply.github.com>
2023-10-28 23:32:44 +0200

    Initial Commit

---
+++ SmoothRoute/SmoothRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmoothRouteApp.swift
+//  SmoothRoute
+//
+//  Created by Klaudiusz Mękarski on 28/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SmoothRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
